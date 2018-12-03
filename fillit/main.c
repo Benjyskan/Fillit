47a354972afaf6c62b1091fa046509b6823af74a
4slan <amalsago@student.42.fr>
2018-12-03 21:03:59 +0100

    checks added to main

--- fillit/main.c
+++ fillit/main.c
@@ -6,14 +6,36 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 20:29:34 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/03 20:30:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/03 20:57:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		main(int argc, char **argv)
+int		main(int ac, char **av)
 {
-	
+	int		fd;
+
+	if (ac != 2)
+	{
+		ft_putendl_fd("usage: ./fillit filename", 2);
+		return (1);
+	}
+	if (!(fd = open(av[1], O_RDONLY)))
+	{
+		ft_putendl_fd("error: while opening file", 2);
+		return (1);
+	}
+	if (!format_check(fd))
+	{
+		ft_putendl_fd("error: bad format", 2);
+		return (1);
+	}
+	if (!pieces_check(fd))
+	{
+		ft_putendl_fd("error: one or more pieces are invalid", 2);
+		return (1);
+	}
+
 	return (0);
 }

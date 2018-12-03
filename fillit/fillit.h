5ba7a6003008412c2fc90dcd18a3c1803d78f599
4slan <amalsago@student.42.fr>
2018-12-03 20:33:23 +0100

    pieces_check() prototyped and int remplaced by unsigned keyword

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:23:25 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/03 20:31:43 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/03 20:32:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "../libft/libft.h"
 
-int		format_check(int fd);
+unsigned int	format_check(int fd);
+unsigned int	pieces_check(int fd);
 
 #endif

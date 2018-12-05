fa77e7ceb522783bcf13e84b5bd44248479320fc
4slan <amalsago@student.42.fr>
2018-12-05 14:34:44 +0100

    prototypes of functions format_check() and piece_check() modified to recieve file in its armunet

--- src/fillit.h
+++ src/fillit.h
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:23:25 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/04 20:49:04 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/05 14:24:32 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ typedef struct		s_tetri_coo
 	struct s_xy		p3;
 }					t_tetri_coo;
 
-unsigned int		format_check(int fd);
-unsigned int		pieces_check(int fd, int tetri_cnt);
+unsigned int		format_check(char *av);
+unsigned int		pieces_check(char *av, int tetri_cnt);
 t_tetri_coo			fill_tetri_coo(char *tetri);
 
 #endif

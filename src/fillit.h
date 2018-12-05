3efe9483cac933b9341e6f7b1a1daa03a52b2b68
4slan <amalsago@student.42.fr>
2018-12-05 17:30:26 +0100

    forget to terminate the line with ;

--- src/fillit.h
+++ src/fillit.h
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:23:25 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/05 17:01:03 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/05 17:06:31 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ typedef struct		s_tetri_coo
 	struct s_xy		p[4];
 }					t_tetri_coo;
 
-int					open_sample(char *av)
+int					open_sample(char *av);
 unsigned int		format_check(char *av);
 unsigned int		pieces_check(char *av, int tetri_cnt);
 t_tetri_coo			fill_tetri_coo(char *tetri);

f2850d84cf6ba3b83309e89dd63c9cc90d6b0884
4slan <amalsago@student.42.fr>
2018-12-05 17:29:30 +0100

    else if forest remplaced with array of s_xy

--- src/fill_tetri_coo.c
+++ src/fill_tetri_coo.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 18:49:11 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/05 16:48:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/05 17:28:05 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,17 @@ t_tetri_coo		fill_tetri_coo(char *str)
 	{
 		if (check == 0 && str[i] == '#')
 		{
-			tetri.p0.original = i;
-			tetri.p0.x = 0;
-			tetri.p0.y = 0;
+			tetri.p[check].original = i;
+			tetri.p[check].x = 0;
+			tetri.p[check].y = 0;
 			check++;
 		}
-		else if (check == 1 && str[i] == '#')
+		else if (str[i] == '#')
 		{
-			tetri.p1.original = i - tetri.p0.original;
-			tetri.p1.x = tetri.p1.original % 5;
-			tetri.p1.y = tetri.p1.original / 5;
+			tetri.p[check].original = i - tetri.p[0].original;
+			tetri.p[check].x = tetri.p[check].original % 5;
+			tetri.p[check].y = tetri.p[check].original / 5;
 			check++;
-		}
-		else if (check == 2 && str[i] == '#')
-		{
-			tetri.p2.original = i - tetri.p0.original;
-			tetri.p2.x = tetri.p2.original % 5;
-			tetri.p2.y = tetri.p2.original / 5;
-			check++;
-		}
-		else if (check == 3 && str[i] == '#')
-		{
-			tetri.p3.original = i - tetri.p0.original;
-			tetri.p3.x = tetri.p3.original % 5;
-			tetri.p3.y = tetri.p3.original / 5;
 		}
 		i++;
 	}

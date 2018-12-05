75aa55d552f66099515acf36f00edf9a22b40228
Piriou ENZO <penzo@e3r1p12.42.fr>
2018-12-05 19:09:53 +0100

    fixed formula, and norm correction

--- src/fill_tetri_coo.c
+++ src/fill_tetri_coo.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 18:49:11 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/05 17:28:05 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/05 19:06:56 by penzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 t_tetri_coo		fill_tetri_coo(char *str)
 {
 	unsigned		i;
-	unsigned		check;
+	unsigned		bloc_cnt;
 	t_tetri_coo		tetri;
 
 	i = 0;
-	check = 0;
+	bloc_cnt = -1;
 	while (str[i])
 	{
-		if (check == 0 && str[i] == '#')
+		if (str[i] == '#' && ++bloc_cnt == 0)
 		{
-			tetri.p[check].original = i;
-			tetri.p[check].x = 0;
-			tetri.p[check].y = 0;
-			check++;
+			tetri.p[bloc_cnt].original = i;
+			tetri.p[bloc_cnt].x = 0;
+			tetri.p[bloc_cnt].y = 0;
 		}
 		else if (str[i] == '#')
 		{
-			tetri.p[check].original = i - tetri.p[0].original;
-			tetri.p[check].x = tetri.p[check].original % 5;
-			tetri.p[check].y = tetri.p[check].original / 5;
-			check++;
+			tetri.p[bloc_cnt].original = i - tetri.p[0].original;
+			tetri.p[bloc_cnt].x = (i % 5) - (tetri.p[0].original % 5);
+			tetri.p[bloc_cnt].y = tetri.p[bloc_cnt].original / 5;
+			if (tetri.p[bloc_cnt].x < 0)
+				tetri.p[bloc_cnt].y++;
 		}
 		i++;
 	}

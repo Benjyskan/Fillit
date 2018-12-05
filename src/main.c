8e2cbb35c01f27a3cf10ee990fb0db5338112740
4slan <amalsago@student.42.fr>
2018-12-05 14:58:43 +0100

    fnctl.h removed

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 20:29:34 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/05 14:28:48 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/05 14:53:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <fcntl.h>
 
 int				main(int ac, char **av)
 {

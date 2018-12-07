9816f95bdbb8380366fe5eb8516f2285cb26c761
4slan <amalsago@student.42.fr>
2018-12-07 18:12:36 +0100

    is_bloc_placeable() definition

--- src/fill_table.c
+++ src/fill_table.c
@@ -6,37 +6,50 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 12:54:58 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/07 17:21:54 by penzo            ###   ########.fr       */
+/*   Updated: 2018/12/07 18:12:09 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+int		is_bloc_placeable(t_tetri_coo tetri_lst, unsigned x, unsigned y, unsigned length)
+{
+	unsigned	bloc_cnt;
+	
+	bloc_cnt = 0;
+	while(bloc_cnt < 4)
+	{
+		if (x + tetri_lst.p[bloc_cnt].x >= 0
+				&& x + tetri_lst.p[bloc_cnt].x < length
+				&& y + tetri_lst.p[bloc_cnt].y >= 0
+				&& y + tetri_lst.p[bloc_cnt].y < length)
+			bloc_cnt++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
 void	fill_square(t_tetri_coo *tetri_lst, char ***tab, unsigned length)
 {
 	unsigned	x;	
 	unsigned	y;	
-	unsigned	bloc_cnt = 0;
-
+	unsigned	i;
+	unsigned	bloc_cnt;
+	
+	bloc_cnt = -1;
 	y = -1;
+	i = 0;
 	while (++y < length)
 	{
 		x = -1;
 		while (++x <= length)
 		{
-			while(bloc_cnt < 4)
+			if (is_bloc_placeable(tetri_lst[i], x, y, length))
 			{
-				if (x + tetri_lst[0].p[bloc_cnt].x >= 0
-						&& x + tetri_lst[0].p[bloc_cnt].x < length
-						&& y + tetri_lst[0].p[bloc_cnt].y >= 0
-						&& y + tetri_lst[0].p[bloc_cnt].y < length)
-				{
-					tab[0][y + tetri_lst[0].p[bloc_cnt].y][x + tetri_lst[0].p[bloc_cnt].x]
-						= '#';
-					bloc_cnt++;
-				}
-				else
-					break;
+				while (++bloc_cnt < 4)
+					tab[0][y + tetri_lst[i].p[bloc_cnt].y][x + tetri_lst[i].p[bloc_cnt].x] = '#';
+				i++;
 			}
 		}
 	}

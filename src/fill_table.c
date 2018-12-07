78a62a0d83647c9773216ba0c00386a0a527cf00
4slan <amalsago@student.42.fr>
2018-12-07 18:31:23 +0100

    call of is_bloc_placeable() corrected and reassign a capital letter for each placed tetriminos

--- src/fill_table.c
+++ src/fill_table.c
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 12:54:58 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/07 18:16:23 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/07 18:21:38 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,24 @@ void	fill_square(t_tetri_coo *tetri_lst, char ***tab, unsigned length)
 {
 	unsigned	x;	
 	unsigned	y;	
-	unsigned	i;
+	unsigned	tetri_cnt;
 	unsigned	bloc_cnt;
 	unsigned	c;
 	
 	c = 65;
 	bloc_cnt = -1;
 	y = -1;
-	i = 0;
+	tetri_cnt = 0;
 	while (++y < length)
 	{
 		x = -1;
 		while (++x <= length)
 		{
-			if (is_bloc_placeable(tetri_lst[i], x, y, length))
+			if (is_bloc_placeable(tetri_lst[tetri_cnt], x, y, length))
 			{
 				while (++bloc_cnt < 4)
-					tab[0][y + tetri_lst[i].p[bloc_cnt].y][x + tetri_lst[i].p[bloc_cnt].x] = (char)c;
-				i++;
+					tab[0][y + tetri_lst[tetri_cnt].p[bloc_cnt].y][x + tetri_lst[tetri_cnt].p[bloc_cnt].x] = (char)c;
+				tetri_cnt++;
 				c++;
 			}
 		}

9079b50d61f88e39b1afdfc7160cc1a441e947c4
4slan <amalsago@student.42.fr>
2018-12-07 12:26:10 +0100

    name of av parameter changed to filename

--- src/fillit.h
+++ src/fillit.h
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:23:25 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/07 09:30:23 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/07 12:25:44 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ typedef struct		s_tetri_coo
 	struct s_xy		p[4];
 }					t_tetri_coo;
 
-int					open_sample(char *av);
-unsigned int		format_check(char *av);
-unsigned int		pieces_check(char *av, unsigned tetri_cnt);
+int					open_sample(char *filename);
+unsigned int		format_check(char *filename);
+unsigned int		pieces_check(char *filename, unsigned tetri_cnt);
 t_tetri_coo			fill_tetri_coo(char *tetri);
 
 #endif

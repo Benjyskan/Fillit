e1eab7f30378b839479b4733053ddc5fc6607219
Piriou ENZO <penzo@e3r2p13.42.fr>
2018-12-07 17:30:30 +0100

    added new functions prototypes

--- src/fillit.h
+++ src/fillit.h
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:23:25 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/07 12:59:36 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/07 16:56:21 by penzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,13 @@ typedef struct		s_tetri_coo
 
 int					open_sample(char *filename);
 unsigned int		format_check(char *filename);
-unsigned int		pieces_check(char *filename, unsigned tetri_cnt);
+t_tetri_coo			*pieces_check(char *filename, unsigned tetri_cnt);
 t_tetri_coo			fill_tetri_coo(char *tetri);
-					create_square(unsigned nb_tetri);
+char				**create_square(unsigned nb_tetri);
+unsigned			ft_sqrt_up(unsigned n);
+char				**resize_square(char **tab, unsigned length);
+void				fill_w_point(char ***tab, unsigned length);
+void				fill_square(t_tetri_coo *tetri_lst,
+					char ***tab, unsigned length);
 
 #endif

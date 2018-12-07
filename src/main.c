aa5bf504f7df290782481f1c23b76bbc446727a3
Piriou ENZO <penzo@e3r2p13.42.fr>
2018-12-07 17:34:27 +0100

    changed main for testing new function

--- src/main.c
+++ src/main.c
@@ -6,15 +6,20 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 20:29:34 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/05 15:13:31 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/07 17:08:19 by penzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <stdio.h>//tejme
 
 int				main(int ac, char **av)
 {
 	unsigned	tetri_cnt;
+	unsigned	length;
+	t_tetri_coo	*tetri_lst;
+	//int			i = 1;//tejme
+	char		**tab;
 
 	if (ac != 2)
 	{
@@ -26,10 +31,26 @@ int				main(int ac, char **av)
 		ft_putendl_fd("error: bad format", 1);
 		return (1);
 	}
-	if (!(pieces_check(av[1], tetri_cnt)))
+	if (!(tetri_lst = pieces_check(av[1], tetri_cnt)))
 	{
 		ft_putendl_fd("error: one or more pieces are invalid", 1);
 		return (1);
 	}
+	/*printf("x=%d y=%d\n", tetri_lst[i].p[0].x, tetri_lst[i].p[0].y);
+	printf("x=%d y=%d\n", tetri_lst[i].p[1].x, tetri_lst[i].p[1].y);
+	printf("x=%d y=%d\n", tetri_lst[i].p[2].x, tetri_lst[i].p[2].y);
+	printf("x=%d y=%d\n\n", tetri_lst[i].p[3].x, tetri_lst[i].p[3].y);*/
+
+	length = ft_sqrt_up(tetri_cnt * 4);
+	tab = create_square(length);
+	//tab = resize_square(tab, length);
+	fill_square(tetri_lst, &tab, length);
+
+	printf("%s\n", tab[0]);
+	printf("%s\n", tab[1]);
+	printf("%s\n", tab[2]);
+	printf("%s\n", tab[3]);
+	printf("%s\n", tab[4]);
+	printf("%s\n", tab[5]);
 	return (0);
 }

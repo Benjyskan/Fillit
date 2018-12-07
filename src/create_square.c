9dfbabe2503688f9da84cb024402c843da06aa9d
Piriou ENZO <penzo@e3r2p13.42.fr>
2018-12-07 17:39:25 +0100

    create_square.c malloc the table, resize it and fill it with '.'

--- src/create_square.c
+++ src/create_square.c
@@ -6,13 +6,76 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 12:55:42 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/07 12:57:11 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/07 17:05:04 by penzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-create_square(unsigned nb_tetri)
-{
-	starting_size;
+#include <string.h>
+#include "fillit.h"
+#include <stdio.h>//tejme
 
-	starting_size = (nb_tetri * 4);
+unsigned	ft_sqrt_up(unsigned n)//n = tetri_cnt * 4
+{
+	unsigned	res;
+
+	res = 0;
+	while (res * res < n)
+		res++;
+	return (res);
 }
+
+/*
+** create or recreate table
+*/
+
+char	**create_square(unsigned length)
+{
+	//unsigned	starting_size;
+	char		**tab;
+	unsigned	i;
+
+	//starting_size = ft_sqrt_up(nb_tetri * 4);
+	if (!(tab = (char**)malloc(sizeof(char*) * length)))
+		return (NULL);
+	i = -1;
+	while (++i < length)
+	{
+		if (!(tab[i] = (char*)malloc(sizeof(char) * length)))
+		{
+			while (i-- > 0)
+			{
+				free(tab[i]);
+				tab[i] = NULL;
+			}
+			free(tab);
+			tab = NULL;
+			return (NULL);
+		}
+	}
+	fill_w_point(&tab, length);
+	return (tab);
+}
+
+char	**resize_square(char **tab, unsigned length)
+{
+	free(tab);
+	create_square(length + 1);
+	return (tab);
+}
+
+void	fill_w_point(char ***tab, unsigned length)
+{
+	unsigned	x;
+	unsigned	y;
+
+	y = -1;
+	while (++y < length)
+	{
+		x = -1;
+		while (++x < length)
+			tab[0][y][x] = '.';
+	}
+	return ;
+}
+
+//create_square(ft_sqrt_up(nb_tetri * 4);

3860f02688a718078e2b9ede719580409f73f6cd
4slan <amalsago@student.42.fr>
2018-12-07 12:47:31 +0100

    av changed to be filename

--- src/open_sample.c
+++ src/open_sample.c
@@ -6,18 +6,18 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 14:44:32 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/05 14:54:14 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/07 12:31:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <fcntl.h>
 
-int			open_sample(char *av)
+int			open_sample(char *filename)
 {
 	int		fd;
 
-	fd = open(av, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd <= 0)
 	{
 		ft_putendl_fd("error: opening file", 1);

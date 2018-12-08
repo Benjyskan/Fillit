150788a62e987905387d0f7d0bf24e9e6ac2eca3
4slan <amalsago@student.42.fr>
2018-12-08 18:08:25 +0100

    empty lines removed

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 20:29:34 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/08 17:03:19 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/08 18:07:53 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <stdio.h>//tejme
 
 int					main(int ac, char **av)
 {
@@ -36,22 +35,14 @@ int					main(int ac, char **av)
 		ft_putendl_fd("error: one or more pieces are invalid", 1);
 		return (1);
 	}
-	/*printf("x=%d y=%d\n", tetri_lst[i].p[0].x, tetri_lst[i].p[0].y);
-	printf("x=%d y=%d\n", tetri_lst[i].p[1].x, tetri_lst[i].p[1].y);
-	printf("x=%d y=%d\n", tetri_lst[i].p[2].x, tetri_lst[i].p[2].y);
-	printf("x=%d y=%d\n\n", tetri_lst[i].p[3].x, tetri_lst[i].p[3].y);*/
-
 	length = ft_sqrt_up(tetri_cnt * 4);
 	tab = create_square(length);
-	//tab = resize_square(tab, length);
 	fill_square(tetri_lst, &tab, &length, tetri_cnt);
-
 	printf("length : %d\n", length);
 	while (i < length)
 	{
 		printf("%s\n", tab[i]);
 		i++;
 	}
-
 	return (0);
 }

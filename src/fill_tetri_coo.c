5d32e7b09736c1a323686d13ee570268c88f42d3
4slan <amalsago@student.42.fr>
2018-12-09 17:06:59 +0100

    b_cnt become bloc_cnt, t_cnt become tetri_cnt as it was

--- src/fill_tetri_coo.c
+++ src/fill_tetri_coo.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 18:49:11 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/09 08:10:33 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/09 17:05:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 t_coo			fill_tetri_coo(char *str)
 {
 	int			i;
-	int			b_cnt;
+	int			bloc_cnt;
 	t_coo		tetri;
 
 	i = 0;
-	b_cnt = -1;
+	bloc_cnt = -1;
 	while (str[i])
 	{
-		if (str[i] == '#' && ++b_cnt == 0)
+		if (str[i] == '#' && ++bloc_cnt == 0)
 		{
-			tetri.p[b_cnt].original = i;
-			tetri.p[b_cnt].x = 0;
-			tetri.p[b_cnt].y = 0;
+			tetri.p[bloc_cnt].original = i;
+			tetri.p[bloc_cnt].x = 0;
+			tetri.p[bloc_cnt].y = 0;
 		}
 		else if (str[i] == '#')
 		{
-			tetri.p[b_cnt].original = i - tetri.p[0].original;
-			tetri.p[b_cnt].x = (i % 5) - (tetri.p[0].original % 5);
-			tetri.p[b_cnt].y = tetri.p[b_cnt].original / 5;
-			if (tetri.p[b_cnt].x < 0)
-				tetri.p[b_cnt].y++;
+			tetri.p[bloc_cnt].original = i - tetri.p[0].original;
+			tetri.p[bloc_cnt].x = (i % 5) - (tetri.p[0].original % 5);
+			tetri.p[bloc_cnt].y = tetri.p[bloc_cnt].original / 5;
+			if (tetri.p[bloc_cnt].x < 0)
+				tetri.p[bloc_cnt].y++;
 		}
 		i++;
 	}

cc46fd17b55e16416a914ae16abc2f2b83020218
4slan <amalsago@student.42.fr>
2018-12-10 22:09:39 +0100

    remove pieces_check() and format_check() prototypes because they are now static

--- src/fillit.h
+++ src/fillit.h
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:23:25 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/10 21:41:42 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/10 22:08:47 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,17 @@ typedef struct		s_xy
 typedef struct		s_coo
 {
 	struct s_xy		p[4];
-	int				c;//gere
+	int				c;
 }					t_coo;
 
 t_coo				*g_tetri_lst;
 int					g_tetri_total;
 
-int					open_sample(char *filename);
-int					format_check(char *filename);
-int					pieces_check(char *filename);
 t_coo				fill_tetri_coo(char *tetri);
-char				**create_square(int nb_tetri);
+int					open_sample(char *filename);
+int					check_file(char *filename);
 int					ft_sqrt_up(int n);
+char				**create_square(int nb_tetri);
 void				resize_square(char ***tab, int *len);
 void				initialise(char ***tab, int len);
 void				fill_square(t_coo *lst, t_table table, int total);

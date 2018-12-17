591065d0c3e9e1bd60f207558b95ec72aaeea3f6
4slan <amalsago@student.42.fr>
2018-12-17 09:38:23 +0100

    Commments normalized; tetr_index renamed into index in is_tetri_placeable() and print_tetri() to be conform with norm; coor_plus() moved in fillit_utils.c; try_map() becomes static; coor->x = 0 and coor->y = 0 now in function coor_reset() in fillit_utils.c

--- sources/fill_square.c
+++ sources/fill_square.c
@@ -6,40 +6,36 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 12:54:58 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/16 19:05:12 by penzo            ###   ########.fr       */
+/*   Updated: 2018/12/17 09:29:26 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
 /*
- ** check if all blocs of the given tetri is placable
- ** check if the character is '.' && on the table
- */
+** check if all blocs of the given tetri is placable
+** check if the character is '.' && on the table
+*/
 
-//static int		is_tetri_placeable(t_table *table,  int x, int y, int tetr_index)
-static int		is_tetri_placeable(t_table *table,  int x, int y, int tetr_index)
+static int		is_tetri_placeable(t_table *table, int x, int y, int index)
 {
-	//ft_putendl("sta is_tetri");
 	int			bloc_cnt;
 
 	bloc_cnt = 0;
 	while (bloc_cnt < 4)
 	{
-		if (x + g_tetri_lst[tetr_index].p[bloc_cnt].x >= 0
-				&& x + g_tetri_lst[tetr_index].p[bloc_cnt].x < table->len
-				&& y + g_tetri_lst[tetr_index].p[bloc_cnt].y >= 0
-				&& y + g_tetri_lst[tetr_index].p[bloc_cnt].y < table->len
-				&& table->tab[y + g_tetri_lst[tetr_index].p[bloc_cnt].y][x
-				+ g_tetri_lst[tetr_index].p[bloc_cnt].x] == '.')
+		if (x + g_tetri_lst[index].p[bloc_cnt].x >= 0
+				&& x + g_tetri_lst[index].p[bloc_cnt].x < table->len
+				&& y + g_tetri_lst[index].p[bloc_cnt].y >= 0
+				&& y + g_tetri_lst[index].p[bloc_cnt].y < table->len
+				&& table->tab[y + g_tetri_lst[index].p[bloc_cnt].y][x
+				+ g_tetri_lst[index].p[bloc_cnt].x] == '.')
 			bloc_cnt++;
 		else
 		{
-			//ft_putendl("end is_tetri");
 			return (0);
 		}
 	}
-	//ft_putendl("end is_tetri");
 	return (1);
 }
 
@@ -71,28 +67,14 @@ static t_pnt	delete_tetri(t_table *table, int c)
 	return (last_pos);
 }
 
-static void		print_tetri(t_table *table, int x, int y, int tetri_index)
+static void		print_tetri(t_table *table, int x, int y, int index)
 {
 	int			bloc_cnt;
 
 	bloc_cnt = -1;
 	while (++bloc_cnt < 4)
-		table->tab[y + g_tetri_lst[tetri_index].p[bloc_cnt].y]
-			[x + g_tetri_lst[tetri_index].p[bloc_cnt].x]
-			= g_tetri_lst[tetri_index].c;
-}
-
-void		coor_plus(t_pnt *coor, int len)
-{
-	if (coor->x < len - 1)
-	{
-		coor->x += 1;
-	}
-	else if (coor->x == len - 1 && coor->y < len - 1)
-	{
-		coor->x = 0;
-		coor->y += 1;
-	}
+		table->tab[y + g_tetri_lst[index].p[bloc_cnt].y]
+			[x + g_tetri_lst[index].p[bloc_cnt].x] = g_tetri_lst[index].c;
 }
 
 static int	place_letter(t_table *table, t_pnt *coor, int index)
@@ -103,7 +85,6 @@ static int	place_letter(t_table *table, t_pnt *coor, int index)
 			return (0);
 		coor_plus(coor, table->len);
 	}
-	//while (coor.x < table->len - 1 && coor.y < table->len - 1)
 	while (!(coor->x >= table->len - 1 && coor->y >= table->len - 1))
 	{
 		if (is_tetri_placeable(table, coor->x, coor->y, index))
@@ -117,40 +98,39 @@ static int	place_letter(t_table *table, t_pnt *coor, int index)
 	return (0);
 }
 
-int			try_map(t_table *table, t_pnt *coor, int index)
+static int			try_map(t_table *table, t_pnt *coor, int index)
 {
-	t_pnt	last_pos;
+	t_pnt			last_pos;
+
 	if (index >= g_tetri_total)
 		return (1);
-	if (coor->x >= table->len - 1 && coor->y >= table->len - 1)//
-		return (0);//
-	//if (is_tetri_placeable(table, coor->x, coor->y, index))
+	if (coor->x >= table->len - 1 && coor->y >= table->len - 1)
+		return (0);
 	if (place_letter(table, coor, index))
 	{
-		coor->x = 0;
-		coor->y = 0;
+		coor_reset(coor);
 		if (try_map(table, coor, ++index))
 			return (1);
 		last_pos = delete_tetri(table, g_tetri_lst[--index].c);
 		coor_plus(&last_pos, table->len);
-		/*while (table->tab[coor->y][coor->x] != '.'
+		/*
+		while (table->tab[coor->y][coor->x] != '.'
 		  && (coor->x < table->len - 1 && coor->y < table->len - 1))
-		  coor_plus(coor, table->len);*/
+		  coor_plus(coor, table->len);
+		*/
 		return (try_map(table, &last_pos, index));
-		//return (try_map(table, coor, index));
 	}
 	return (0);
 }
 
-void		fillit(t_table *table, t_pnt *coor)
+void				fillit(t_table *table, t_pnt *coor)
 {
 	//if (i >= g_tetri_total)
-	//return ;
+	//	return ;
 	if (try_map(table, coor, 0))
 		return ;
 	resize_square(table);
-	coor->x = 0;
-	coor->y = 0;
+	coor_reset(coor);
 	fillit(table, coor);
 	return ;
 }

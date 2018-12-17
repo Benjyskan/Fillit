c2eeb0869f788fd30be145e45349eb12c65cd8e8
4slan <amalsago@student.42.fr>
2018-12-17 14:13:15 +0100

    add check where program try to read 0 bytes to avoid error with directory in argument. And add check if g_tetri_total is more than 26, so we need to stop program

--- sources/checks.c
+++ sources/checks.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 20:50:33 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/17 12:33:00 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/17 14:11:20 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,6 +118,8 @@ static int			format_check(char *filename)
 
 	if ((fd = open(filename, O_RDONLY)) == -1)
 		return (fd);
+	if (read(fd, buff, 0) < 0)
+		return (0);
 	while ((y = read(fd, buff, 21)) > 0)
 	{
 		if (y < 20)
@@ -135,6 +137,8 @@ static int			format_check(char *filename)
 			return (0);
 		g_tetri_total++;
 	}
+	if (g_tetri_total > 26)
+		return (0);
 	close(fd);
 	return (1);
 }

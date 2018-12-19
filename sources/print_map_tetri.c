fe3b17c328268f857773304bbbcdfc79899278c6
4slan <amalsago@student.42.fr>
2018-12-19 16:27:16 +0100

    put assignement in one line

--- sources/print_map_tetri.c
+++ sources/print_map_tetri.c
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 11:29:00 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/18 19:12:42 by penzo            ###   ########.fr       */
+/*   Updated: 2018/12/19 14:37:56 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,5 @@ void		print_tetri(t_table *table, t_pnt *coor, int index, t_tetri *tetri)
 	bloc_cnt = -1;
 	while (++bloc_cnt < 4)
 		table->tab[coor->y + tetri->lst[index].p[bloc_cnt].y]
-			[coor->x + tetri->lst[index].p[bloc_cnt].x]
-			= tetri->lst[index].c;
+			[coor->x + tetri->lst[index].p[bloc_cnt].x] = tetri->lst[index].c;
 }

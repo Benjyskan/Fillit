2783620d3b1df1a8763de13c649f8632b6f3d690
4slan <amalsago@student.42.fr>
2018-12-19 12:19:26 +0100

    old function using global variable removed

--- sources/checks.c
+++ sources/checks.c
@@ -6,12 +6,12 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 20:50:33 by amalsago          #+#    #+#             */
-/*   Updated: 2018/12/18 19:17:43 by penzo            ###   ########.fr       */
+/*   Updated: 2018/12/19 12:04:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
 #include <unistd.h>
+#include "fillit.h"
 
 /*
 ** Check tetriminos shapes by counting the number of touching sides
@@ -48,32 +48,6 @@ static int			isvalid_tetri(char *tetri)
 ** pieces_check() verify if pieces are valid and allocate an array to stock
 ** each pieces by calling fill_tetri_coo()
 */
-
-/*static int			pieces_check(char *filename)
-{
-	int				i;
-	int				fd;
-	char			buff[21];
-
-	if ((fd = open(filename, O_RDONLY)) == -1)
-		return (fd);
-	if (!(g_tetri_lst = (t_coo *)malloc(sizeof(t_coo) * g_tetri_total)))
-	{
-		ft_putendl("error");
-		return (0);
-	}
-	i = 0;
-	while (read(fd, buff, 21) > 0)
-	{
-		buff[20] = 0;
-		if (!isvalid_tetri(buff))
-			return (0);
-		g_tetri_lst[i] = fill_tetri_coo(buff);
-		g_tetri_lst[i].c = i + 'A';
-		i++;
-	}
-	return ((close(fd) == -1) ? -1 : 1);
-}*/
 
 static int			pieces_check(char *filename, t_tetri *tetri)
 {
@@ -135,34 +109,6 @@ static int			bloc_cnt(char *buff)
 ** newline and if there are only 4 sharp (#) per tetriminos by bloc_cnt()
 */
 
-/*static int			format_check(char *filename)
-{
-	int				y;
-	int				fd;
-	char			buff[21];
-
-	if ((fd = open(filename, O_RDONLY)) == -1 || read(fd, buff, 0) == -1)
-		return (-1);
-	while ((y = read(fd, buff, 21)) > 0)
-	{
-		if (y == 20)
-		{
-			if (!bloc_cnt(buff))
-				return (0);
-			g_tetri_total++;
-			break ;
-		}
-		if (y < 20 || buff[20] != '\n' || !bloc_cnt(buff))
-			return (0);
-		g_tetri_total++;
-	}
-	if (y == 0 && g_tetri_total)
-		return (0);
-	if (close(fd) == -1)
-		return (-1);
-	return (g_tetri_total != 0 && g_tetri_total < 27 ? 1 : 0);
-}*/
-
 static int			format_check(char *filename, t_tetri *tetri)
 {
 	int				y;
@@ -189,7 +135,6 @@ static int			format_check(char *filename, t_tetri *tetri)
 	if (close(fd) == -1)
 		return (-1);
 	return (1);
-	//return (g_tetri_total != 0 && g_tetri_total < 27 ? 1 : 0);
 }
 
 /*

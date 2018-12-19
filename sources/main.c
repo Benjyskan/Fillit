e3369b95dae649b79fb07de33e45c0bb0cc834f2
4slan <amalsago@student.42.fr>
2018-12-19 17:04:51 +0100

    use exit() if check_file() or create_square() returns bad value

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 17:32:08 by penzo             #+#    #+#             */
-/*   Updated: 2018/12/19 12:05:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2018/12/19 17:04:33 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int				main(int ac, char **av)
 		return (1);
 	}
 	if (check_file(av[1], &tetri) <= 0)
-		return (1);
+		exit(1);
 	coor_reset(&coor);
 	table.len = ft_sqrt_up(tetri.total * 4);
 	if (!(table.tab = create_square(table.len)))
-		return (1);
+		exit(1);
 	fillit(&table, &coor, &tetri);
 	print_map(&table);
 	freellit(&table, &tetri);
